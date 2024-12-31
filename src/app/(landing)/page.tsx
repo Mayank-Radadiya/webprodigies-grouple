1ac1796400cd041874e5eb373ddb0630cfe34dc6
Mayank-Radadiya <mayankradadiya2@gmail.com>
2024-12-31 16:40:48 +0530

    complete LandingPage

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,12 +1,16 @@
 import CallToAction from "./_components/call-to-action/CallToAction"
+import DashboardSnippet from "./_components/DashboardSnippet/DashboardSnippet"
+import PricingSection from "./_components/Pricing/PricingSection"
 
 export default function Home() {
     return (
         <>
-            <main className="md:px-10 py-20 flex flex-col gap-36">
+            <main className="md:px-10 py-20 flex flex-col gap-28">
                 <div>
                     <CallToAction />
+                    <DashboardSnippet />
                 </div>
+                <PricingSection />
             </main>
         </>
     )
---
+++ src/components/global/backdrop-gradient/BackdropGradient.tsx
@@ -0,0 +1,34 @@
+import { cn } from "@/lib/utils"
+import { FC } from "react"
+
+interface BackdropGradientProps {
+    children: React.ReactNode
+    className?: string
+    container?: string
+    gradientStyle?: "frosted-glass" | "gradient-glow" | "soft-glow"
+}
+
+const BackdropGradient: FC<BackdropGradientProps> = ({
+    children,
+    className = "",
+    container = "",
+    gradientStyle = "frosted-glass",
+}) => {
+    return (
+        <div className={cn("relative flex w-full flex-col", container)}>
+            <div
+                className={cn(
+                    "absolute rounded-[50%] mx-10",
+                    gradientStyle,
+                    className,
+                )}
+                // role="presentation"
+                // aria-hidden="true"
+            >
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default BackdropGradient
